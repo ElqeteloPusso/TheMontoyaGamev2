68859af1d5e32556ad08f6a8d05cb419f59443f0
ElqeteloPusso <83109717+ElqeteloPusso@users.noreply.github.com>
2021-04-24 02:27:30 -0400

    Add files via upload

---
+++ stage3-2.php
@@ -0,0 +1,31 @@
+<?php
+  
+      $pregunta1 = $_POST ["PreguntaUno"];
+      $pregunta2 = $_POST ["PreguntaDos"];
+      $pregunta3 = $_POST ["PreguntaTres"];
+      $pregunta4 = $_POST ["PreguntaCuatro"];
+      $pregunta5 = $_POST ["PreguntaCinco"];
+      $pregunta6 = $_POST ["PreguntaSeis"];
+      $pregunta7 = $_POST ["PreguntaSiete"];
+      $pregunta8 = $_POST ["PreguntaOcho"];
+      $pregunta9 = $_POST ["PreguntaNueve"];
+      $pregunta10 = $_POST ["PreguntaDiez"];
+      $pregunta11 = $_POST ["PreguntaOnce"];
+      $pregunta12 = $_POST ["PreguntaDoce"];
+      $pregunta13 = $_POST ["PreguntaTrece"];
+
+      $mensaje = "";
+
+      $puntos = 0;
+
+      if ($pregunta1 == "v") {
+      	# code...
+      }
+
+
+
+
+
+
+
+?>
